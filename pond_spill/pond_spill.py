5f3ca1679644ca54ab5ce3eb076f1c61b2608964
joanna-chen <joannachen.x@gmail.com>
2015-10-23 22:55:04 -0400

    updated pond spill assignment

--- pond_spill/pond_spill.py
+++ pond_spill/pond_spill.py
@@ -10,7 +10,7 @@ poll1 = 0
 poll2 = 0
 poll3 = 0
 pollLeak = 0
-poll = {0: [0, 0, 0]}
+poll = {0: [0, 0, 0, 0]}
 
 ## Create the graphics window and get the canvas.
 win = GraphicsWindow(500, 500)
@@ -20,41 +20,42 @@ RATE_BETWEEN_POND = 0.005
 def main():
     createGrid(canvas, minutes, int(pollMax))
     for i in range(1, minutes):
-        poll[i] = [pond1(i), pond2(i), pond3(i)]
+        poll[i] = [pond1(i), pond2(i), pond3(i), pollutantLeak(i)]
     for i in range(60, minutes, 60):
         if i < minutes:
             print('Minutes: ', i)
         else:
             print('Minutes [FINAL]: ', i)
-        print('Pond 1: ', pond1(i), ' Pond 2: ', pond2(i), ' Pond 3: ', pond3(i))
+        print('Pond 1: ', poll[i][0], ' Pond 2: ', poll[i][1], ' Pond 3: ', poll[i][2])
     if minutes <= 1440:
         print('Minutes: 1440')
         print('Pond 1: ', pond1(1440), ' Pond 2: ', pond2(1440), ' Pond 3: ', pond3(1440))
         
 def pollutantLeak(t):
     pollLeak = t * pollRate
-    if pollLeak > pollMax:
-        pollLeak = pollMax
-    return pollLeak
+    if pollLeak < pollMax:
+    	return pollLeak - (t-1)*pollRate
+    else:
+    	return 0
 
 def pond1(t):
     inflow3 = RATE_BETWEEN_POND*poll[t-1][2]
     outflow2 = RATE_BETWEEN_POND*poll[t-1][0]
-    poll1 = inflow3 - outflow2 + pollutantLeak(t)
+    poll1 = poll[t-1][0] + inflow3 - outflow2 + pollutantLeak(t)
     drawDots(canvas, t, minutes, poll1, pollMax, "red")
     return round(poll1, 4)
 
 def pond2(t):
     inflow1 = RATE_BETWEEN_POND*poll[t-1][0]
     outflow3 = RATE_BETWEEN_POND*poll[t-1][1]
-    poll2 = inflow1 - outflow3
+    poll2 = poll[t-1][1] + inflow1 - outflow3
     drawDots(canvas, t, minutes, poll1, pollMax, "green")
     return round(poll2, 4)
 
 def pond3(t):
     inflow2 = RATE_BETWEEN_POND*poll[t-1][1]
     outflow1 = RATE_BETWEEN_POND*poll[t-1][2]
-    poll3 = inflow2 - outflow1
+    poll3 = poll[t-1][2] + inflow2 - outflow1
     drawDots(canvas, t, minutes, poll1, pollMax, "blue")
     return round(poll3, 4)
 
